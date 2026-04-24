e84ae8b1ae14e508773b551cfc7221c39b06ccde
geydicastro-wmd <geydi.castro@wmdcr.com>
2026-04-24 16:15:17 -0600

    helmet reomved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./scss/index.scss";
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,12 @@
-import AppNavbar from '../components/Navbar'
-import HeroSlider from '../components/HeroSlider'
-import Intro from '../components/Intro'
-
-
-export default function Home() {
-    return (
-        <>
-          <AppNavbar />
-          <HeroSlider />
-          <Intro />
-        </>
-    )
+export default function About() {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <h1 className="mb-3">About</h1>
+        <p className="mb-0">
+          This page is ready for your company or brand story content.
+        </p>
+      </div>
+    </section>
+  );
 }
